934b26d739e91d54ae04a894d7036f3e1ae9feab
lwrgrs <lwrogers@ncsu.edu>
2022-01-26 16:33:25 -0500

    added additional analyses for ranking of species

--- Expt_1_Rank_Sum_Analysis.R
+++ Expt_1_Rank_Sum_Analysis.R
@@ -110,3 +110,125 @@ d_rank_sums_sd <- d_rank_sums %>% # calculate deviation of each cultivar from gr
   mutate(deviation = (difference/sd(rank_sum))*2)
 
 shapiro.test(d_rank_sums_sd$deviation) # test for normality
+
+###
+### combined years
+###
+
+dat_2021 <- 
+  read.xlsx("Expt1_LowGap_Cultivar_AUDPC_2021.xlsx", 1) # read in data
+
+dat_2020 <- 
+  data.frame(read.xlsx("Expt1_LowGap_Cultivar_Ratings_2020.xlsx", 1)) # read in data
+
+dat_2020$year <- rep(2020, times = 1050)
+dat_2021$year <- rep(2021, times = 1050)
+
+dat_2020 <- dat_2020 %>%
+  mutate(Cultivar = Treatment.) %>%
+  select(year, Date, Species, Cultivar, Rep, pleaf, pdefol, pstem)
+  
+dat_2021 <- dat_2021 %>%
+  select(year, Date, Species, Cultivar, Rep, pleaf, pdefol, pstem)
+
+dat_combined <- rbind(dat_2020, dat_2021)
+
+str(dat_combined)
+
+dat_combined$year <- as.factor(dat_combined$year)
+dat_combined$Species <- as.factor(dat_combined$Species)
+dat_combined$Cultivar <- as.factor(dat_combined$Cultivar)
+dat_combined$Rep <- as.factor(dat_combined$Rep)
+dat_combined$pleaf <- as.numeric(dat_combined$pleaf)
+dat_combined$pdefol <- as.numeric(dat_combined$pdefol)
+dat_combined$pstem <- as.numeric(dat_combined$pstem)
+
+# calculate rank sums
+d <- dat_combined %>%
+  filter(is.na(pleaf) != T,
+         is.na(pstem) != T,
+         is.na(pdefol) != T) %>%
+  group_by(Cultivar) %>% # calculate mean severity for each cultivar at each date
+  summarise(mean_pleaf = mean(pleaf),
+            mean_pstem = mean(pstem),
+            mean_pdefol = mean(pdefol)) %>%
+  arrange(mean_pleaf) %>%
+  mutate(rank_pleaf = rep(1:21)) %>%
+  arrange(mean_pstem) %>%
+  mutate(rank_pstem = rep(1:21)) %>%
+  arrange(mean_pdefol) %>%
+  mutate(rank_pdefol = rep(1:21))
+
+d_rank_sums <- d %>% # calculate rank sum for each cultivar
+  group_by(as.factor(mean_pleaf)) %>%
+  mutate(new_rank_pleaf = mean(rank_pleaf)) %>%
+  group_by(as.factor(mean_pstem)) %>%
+  mutate(new_rank_pstem = mean(rank_pstem)) %>%
+  group_by(as.factor(mean_pdefol)) %>%
+  mutate(new_rank_pdefol = mean(rank_pdefol)) %>%
+  select(Cultivar, new_rank_pdefol, new_rank_pleaf, new_rank_pstem) %>%
+  group_by(Cultivar) %>%
+  summarise(rank_sum = new_rank_pleaf + new_rank_pstem + new_rank_pdefol)
+
+mean(d_rank_sums$rank_sum) # grand mean: 33
+
+d_rank_sums_sd <- d_rank_sums %>% # calculate deviation of each cultivar from grand mean
+  mutate(difference = rank_sum - 33) %>%
+  mutate(deviation = (difference/sd(rank_sum))*2)
+
+shapiro.test(d_rank_sums_sd$deviation) # test for normality
+
+###
+### ranking species rather than cultivar
+###
+
+levels(dat_combined$Species)
+levels(dat_combined$Cultivar)
+
+dat_combined_2 <- dat_combined %>%
+  mutate(Species = replace(as.character(Species), 
+                           Cultivar == 'Sprinter' & Species == 'Bmicrophyla', 
+                           'BmicroJap')) %>%
+  mutate(Species = replace(as.character(Species), 
+                           Cultivar == 'Franklins Gem' & Species == 'Bmicrosinica', 
+                           'Bsinicainsularis')) %>%
+  mutate(Species = replace(as.character(Species), 
+                           Cultivar == 'Golden Dream' & Species == 'BmicroJap', 
+                           'Bmicrophyla'))
+
+dat_combined_2$Species <- as.factor(dat_combined_2$Species)
+
+# calculate rank sums
+d <- dat_combined_2 %>%
+  filter(is.na(pleaf) != T,
+         is.na(pstem) != T,
+         is.na(pdefol) != T) %>%
+  group_by(Species) %>% # calculate mean severity for each species group at each date
+  summarise(mean_pleaf = mean(pleaf),
+            mean_pstem = mean(pstem),
+            mean_pdefol = mean(pdefol)) %>%
+  arrange(mean_pleaf) %>%
+  mutate(rank_pleaf = rep(1:7)) %>%
+  arrange(mean_pstem) %>%
+  mutate(rank_pstem = rep(1:7)) %>%
+  arrange(mean_pdefol) %>%
+  mutate(rank_pdefol = rep(1:7))
+
+d_rank_sums <- d %>% # calculate rank sum for each species
+  group_by(as.factor(mean_pleaf)) %>%
+  mutate(new_rank_pleaf = mean(rank_pleaf)) %>%
+  group_by(as.factor(mean_pstem)) %>%
+  mutate(new_rank_pstem = mean(rank_pstem)) %>%
+  group_by(as.factor(mean_pdefol)) %>%
+  mutate(new_rank_pdefol = mean(rank_pdefol)) %>%
+  select(Species, new_rank_pdefol, new_rank_pleaf, new_rank_pstem) %>%
+  group_by(Species) %>%
+  summarise(rank_sum = new_rank_pleaf + new_rank_pstem + new_rank_pdefol)
+
+mean(d_rank_sums$rank_sum) # grand mean: 12
+
+d_rank_sums_sd <- d_rank_sums %>% # calculate deviation of each cultivar from grand mean
+  mutate(difference = rank_sum - 12) %>%
+  mutate(deviation = (difference/sd(rank_sum))*2)
+
+shapiro.test(d_rank_sums_sd$deviation) # test for normality
